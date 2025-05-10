b9db525efa656ab4b8638855f06e62400e7aec96
yiyi <DMB-codegang@users.noreply.github.com>
2025-05-10 18:17:27 +0800

    feat: 更新插件版本并添加连续签到天数功能
    更新插件版本至2.0.3，并新增连续签到天数功能。重构了时间服务模块，优化了签到逻辑，增加了用户连续签到天数的计算和展示。同时更新了插件的描述和用法说明，使其更易于理解和使用。

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,21 +1,9 @@
 import { Context } from 'koishi';
 import { Config } from './config';
 export declare const name = "codegang-qd";
-export declare const description = "Codegang\u7B7E\u5230\u63D2\u4EF6";
+export declare const description = "\u4E00\u4E2A\u9AD8\u5EA6\u53EF\u81EA\u5B9A\u4E49\u5316\u7684\u7B7E\u5230\u63D2\u4EF6";
 export declare const author = "\u5C0F\u820D";
-export declare const version = "2.0.0";
 export declare const inject: string[];
+export declare const usage = "\n# \u7B7E\u5230\u63D2\u4EF6\n## \u4ECB\u7ECD\n\n## \u7B7E\u5230\u81EA\u5B9A\u4E49\u6587\u672C\u53D8\u91CF\u4E00\u89C8\n- `{AT}`\u4E3A\u827E\u7279\u7528\u6237  \n- `{username}`\u4E3A\u7528\u6237\u6635\u79F0  \n- `{points}`\u4E3A\u7528\u6237\u83B7\u5F97\u7684\u79EF\u5206  \n- `{fortune}`\u4E3A\u7528\u6237\u8FD0\u52BF  \n- `{consecutive_days}`\u4E3A\u7528\u6237\u8FDE\u7EED\u7B7E\u5230\u5929\u6570\n- `{time}`\u4E3A\u7528\u6237\u7B7E\u5230\u65F6\u95F4  \n- `{totalpoints}`\u4E3A\u7528\u6237\u603B\u79EF\u5206  \n- `{<key>}`\u5176\u4ED6\u7ED1\u5B9A\u503C  \n";
 export * from './config';
-declare module 'koishi' {
-    interface Tables {
-        codegang_qd: codegang_qd;
-    }
-}
-export interface codegang_qd {
-    id: number;
-    userid: string;
-    username: string;
-    time: Date;
-    monthlyRecords: JSON;
-}
 export declare function apply(ctx: Context, cfg: Config): Promise<void>;
